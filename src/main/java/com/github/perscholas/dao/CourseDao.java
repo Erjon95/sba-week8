289e387b01dbc8b36739180b6d080d1aad88a55c
Erjon Dervishaj <er_djs@yahoo.com>
2020-02-16 23:27:21 -0500

    I have made changes

--- src/main/java/com/github/perscholas/dao/CourseDao.java
+++ src/main/java/com/github/perscholas/dao/CourseDao.java
@@ -1,6 +1,8 @@
 package com.github.perscholas.dao;
 
 import com.github.perscholas.model.CourseInterface;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
@@ -8,6 +10,7 @@ import java.util.List;
  * @author leonhunter
  * @created 02/12/2020 - 5:56 PM
  */
-public interface CourseDao {
+public interface CourseDao extends CrudRepository<CourseInterface, Integer> {
+    @Query(value = "select * from Course c", nativeQuery = true)
     List<CourseInterface> getAllCourses();
 }
